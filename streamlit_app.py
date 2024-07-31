f052585f752a33519b804c17945b9868717ffdd1
Jorge Pinzon <jpinzon@hotmail.com>
2024-07-30 20:06:07 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,7 +22,7 @@ with st.expander('Data'):
 
 with st.expander('Data visualization'):
   st.scatter_chart(data=df, x='bill_length_mm', y='body_mass_g', color='species')
-
+  st.scatter_chart(data=input_df, x='bill_length_mm', y='body_mass_g', color='species')
 # Input features
 with st.sidebar:
   st.header('Input features')
